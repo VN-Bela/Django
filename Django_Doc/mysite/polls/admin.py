34d160fac84b51b0dc6a542496f5670458a64ab9
VN-Bela <bela.vnurture@gmail.com>
2021-07-21 09:40:44 +0530

    admin site changes

--- Django_Doc/mysite/polls/admin.py
+++ Django_Doc/mysite/polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Question
-admin.site.register(Question)
+from .models import Question,Choice
 
 # Register your models here.
+
+#class QuestionAdmin(admin.ModelAdmin):
+ #   fieldsets=[
+  ###]
+   
+class ChoiceInline(admin.TabularInline):
+    model=Choice
+    extra=3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets=[
+        (None,{'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes':['collapse']}),
+    
+    ]
+    list_filter = ['pub_date']
+    list_display=('question_text','pub_date','was_published_recently')
+    inlines=[ChoiceInline]
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
--- Django_Doc/mysite/polls/models.py
+++ Django_Doc/mysite/polls/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models.fields import DateTimeField
 from django.utils import  timezone
 import  datetime
+from django.contrib import admin
 # Create your models here.
 
 class Question(models.Model):
@@ -17,6 +18,10 @@ class Question(models.Model):
         now=timezone.now()
         return now-datetime.timedelta(days=1)<=self.pub_date<= now
 
+    @admin.display(boolean=True,ordering='pub_date',description='published recently?',)
+    def was_published_recently(self):
+        now=timezone.now()
+        return now-datetime.timedelta(days=1)<=self.pub_date<=now
 class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
