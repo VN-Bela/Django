f04e71dc3fd99db531c4394f73817e3fa08ff77f
VN-Bela <bela.vnurture@gmail.com>
2021-07-22 16:10:59 +0530

    Static File & Media  And Migration

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for crm1 project.
 
@@ -11,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/images/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
